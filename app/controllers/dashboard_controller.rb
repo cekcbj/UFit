ca61b9ea4e87720fdd61c095e697eaae877bf0f2
Byronj <cekcbj@gmail.com>
2015-10-28 22:21:55 -0500

    workout controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    @workouts = Workout.all
   end
 end
---
+++ test/helpers/workouts_helper_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class WorkoutsHelperTest < ActionView::TestCase
+  def test_sanity
+    flunk "Need real tests"
+  end
+end
